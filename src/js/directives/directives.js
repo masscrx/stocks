c6b6736a4bda0a1d70cbda23949f5c572fc8c157
Marek Bozych <m.bozych@gmail.com>
2017-01-13 19:37:15 +0100

    ADD : manage-button directive

--- src/js/directives/directives.js
+++ src/js/directives/directives.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var ChartDirective = require('./chart/chart.directive');
+var ManageButtonDirective = require('./manage-button/manage-button.directive');
 
 angular.module('app.directives', [])
-.directive('lineChart', ChartDirective);
+.directive('lineChart', ChartDirective)
+.directive('manageButton', ManageButtonDirective);
---
+++ src/js/directives/manage-button/manage-button.directive.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = function() {
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: 'directives/manage-button/manage-button.html',
+    link: function(scope, elem, attrs) {
+    }
+  };
+};
